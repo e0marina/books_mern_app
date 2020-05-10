4dc7c423a0f0e2649d26a6135b06d27c05c8321c
Emily M <30880308+e0marina@users.noreply.github.com>
2020-05-10 12:02:45 -0700

    initial files

---
+++ create-react-express/client/src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// This method retrieves books from the server
+// It accepts a "query" or term to search the api for
+
+export default {
+  getSearchedBook: function (query) {
+    return axios.get("/api/books", { params: { q: query } }, "maxResults=10");
+  },
+};
