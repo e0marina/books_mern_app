96c6c389f1267c7e8f539df95689eb7fc88e8822
Emily M <30880308+e0marina@users.noreply.github.com>
2020-05-11 13:30:22 -0700

    updated files so starting to run and render properly

---
+++ books_mern/client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Books from "./pages/Books";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Books />
+      </Router>
+    </>
+  );
+}
+
+export default App;
+
+{
+  /* <Router>
+<Nav />
+<Books />
+<Switch>
+  <Route exact path="/" component={Books} />
+  <Route exact path="/books" component={Books} />
+  <Route path="*" component={NoMatch} />
+  <Route exact path="/books:id" component={Detail} />
+</Switch>
+</Router> */
+}
