4a1bbbb3fd68aac22feb16f069069ef5814d7a5e
Emily M <30880308+e0marina@users.noreply.github.com>
2020-05-31 10:45:17 -0700

    deleted ref to heroku in build command and deleted env file

---
+++ books_mern/client/build/precache-manifest.cac46f06da0551e89b8c29a5ba76a3a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dfe808dbc9380cf84b5377e445eb8ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "836c8e6e33f36d07e956",
+    "url": "/static/css/main.d9db24cb.chunk.css"
+  },
+  {
+    "revision": "58fb26a09992df3aae89",
+    "url": "/static/js/2.0d052fc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0d052fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "836c8e6e33f36d07e956",
+    "url": "/static/js/main.adf4cb61.chunk.js"
+  },
+  {
+    "revision": "57ebf18190a566828e8d",
+    "url": "/static/js/runtime-main.f0bd043f.js"
+  }
+]);
